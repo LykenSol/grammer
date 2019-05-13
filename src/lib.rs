0ea57c2d8dc6d86769832374f515d02f61f1e7bd
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2019-05-13 19:45:28 +0300

    Move the bootstrap/meta-grammar from the gll crate.

--- src/lib.rs
+++ src/lib.rs
@@ -561,3 +561,111 @@ impl<Pat> RuleWithNamedFields<Pat> {
         rule
     }
 }
+
+/// Construct a (meta-)grammar for parsing a grammar.
+pub fn grammar_grammar<Pat>() -> Grammar<Pat>
+where
+    Pat: Clone
+        + From<&'static str>
+        + From<std::ops::RangeInclusive<char>>
+        + From<std::ops::RangeFull>,
+{
+    // HACK(eddyb) more explicit subset of the grammar, for bootstrapping.
+    macro_rules! rule {
+        ({ $start:tt ..= $end:tt }) => {
+            eat($start..=$end)
+        };
+        ({ ! $pat:tt }) => {
+            negative_lookahead($pat)
+        };
+        ({ ! $start:tt ..= $end:tt }) => {
+            negative_lookahead($start..=$end)
+        };
+        ($rule:ident) => {
+            call(stringify!($rule))
+        };
+        ({ $name:ident : $rule:tt }) => {
+            rule!($rule).field(stringify!($name))
+        };
+        ({ $rule:tt ? }) => {
+            rule!($rule).opt()
+        };
+        ({ $elem:tt * }) => {
+            rule!($elem).repeat_many(None)
+        };
+        ({ $elem:tt + }) => {
+            rule!($elem).repeat_more(None)
+        };
+        ({ $elem:tt + % $sep:tt }) => {
+            rule!($elem).repeat_more(Some(rule!($sep)))
+        };
+        ({ $rule0:tt $(| $rule:tt)+ }) => {
+            rule!($rule0) $(| rule!($rule))+
+        };
+        ({ $rule0:tt $($rule:tt)* }) => {
+            rule!($rule0) $(+ rule!($rule))*
+        };
+        ($pat:expr) => {
+            eat($pat)
+        };
+    }
+
+    macro_rules! grammar {
+        ($($rule_name:ident = $($rule:tt)|+;)*) => ({
+            let mut grammar = Grammar::new();
+            $(grammar.define(stringify!($rule_name), rule!({ $($rule)|+ }));)*
+            grammar
+        })
+    }
+
+    let mut grammar = grammar! {
+        // Lexical grammar.
+        Whitespace = {
+            {{
+                " " | "\t" | "\n" | "\r" |
+                { "//" {{ {!"\n"} .. }*} "\n" } |
+                { "/*" {{ {!"*/"} .. }*} "*/" }
+            }*}
+            {!" "} {!"\t"} {!"\n"} {!"\r"} {!"//"} {!"/*"}
+        };
+        Shebang = { "#!" {{ {!"\n"} .. }*} "\n" };
+
+        IdentStart = {'a'..='z'} | {'A'..='Z'} | "_";
+        IdentCont = IdentStart | {'0'..='9'};
+        NotIdent = { {!'a'..='z'} {!'A'..='Z'} {!"_"} {!'0'..='9'} };
+        Ident = { IdentStart {IdentCont*} NotIdent };
+
+        StrLit = { "\"" {{ { {!"\\"} {!"\""} .. } | { "\\" Escape } }*} "\"" };
+        CharLit = { "'" { { {!"\\"} {!"'"} .. } | { "\\" Escape } } "'" };
+        Escape = "t" | "n" | "r" | "\\" | "'" | "\"";
+    };
+
+    grammar.extend(
+        grammar! {
+            // Main grammar.
+            Grammar = { {Shebang?} {rules:{RuleDef*}} Whitespace };
+            RuleDef = { {name:Ident} "=" {rule:Or} ";" };
+            Or = {{"|"?} {rules:{Concat+ % "|"}}};
+            Concat = {rules:{Rule+}};
+            Rule = { {{ {field:Ident} ":" }?} {rule:Primary} {{modifier:Modifier}?} };
+            Primary =
+                {Eat:Pattern} |
+                {NegativeLookahead:{ "!" {pat:Pattern} }} |
+                {Call:Ident} |
+                {Group:{ "{" {{or:Or}?} "}" }};
+            Modifier =
+                {Opt:"?"} |
+                {Repeat:{ {repeat:Repeat} {{ "%" {sep:Primary} }?} }};
+            Repeat =
+                {Many:"*"} |
+                {More:"+"};
+            Pattern =
+                {Str:StrLit} |
+                {CharRange:{ {{start:CharLit}?} ".." {{end:CharLit}?} }} |
+                {CharRangeInclusive:{ {{start:CharLit}?} "..=" {end:CharLit} }};
+        }
+        .insert_whitespace(call("Whitespace")),
+    );
+
+    grammar
+}
